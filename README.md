b93796dd58ee509b8d7e7cff3084c9a6d0426b02
Niamh O'Flaherty <oflhaerty.niamh@gmail.com>
2018-12-01 22:43:38 +0000

    client updated with semaphore. Readme added

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# bash-assignment
+# bash-assignment-Database Management Server
+
+The DBMS allows a client to create a database, create a table in that database, insert data into tables and to select data to review.
+
+# Run the server script to handle the commands
+
+./server.sh
+
+# Run the client script with a client ID. Within this script commands can be inserted for execution by the server
+
+./client.sh ID
+
+# Command scripts can be run outside of the server
+
+# Create a database with a unique database name
+
+./create_database.sh DATABASENAME
+
+# Create a table with an existing database name, unique table name and headings for columns, separated by columns with no spaces
+
+./create_table.sh DATABASENAME TABLENAME HEADING1,HEADING2,HEADING3
+
+# Insert data into a table by specifying an existing database, an existing table and data to be inserted - this data must be of the same number as the headings
+
+./insert.sh DATABASENAME TABLENAME INSERT1,INSERT2,INSERT3
+
+# Select rows and columns from an existing table in an existing database. This can be done in several ways.
+
+    # Select all columns and rows from an existing table
+
+    ./select.sh DATABASENAME TABLENAME
+
+    # Select specific columns from an existing table
+
+    ./select.sh DATABASENAME TABLENAME 1,3
+
+    # Select specific rows from all the columns in an existing table based on a WHERE clause that matches a value. WHERE is expressed as a column number and rows that match the value will be returned.
+
+    ./select.sh DATABASENAME TABLENAME 1 VALUE
+
+     # Select specific rows from selected columns in an existing table based on a WHERE clause that matches a value. WHERE is expressed as a column number and rows that match the value will be returned.
+
+     ./select.sh DATABASENAME TABLENAME 1,3 1 VALUE
+
+# Clean allows quick removal of any locks, pipes or background servers/clients
+
+./clean.sh
--- client.sh
+++ client.sh
@@ -8,17 +8,29 @@ function ctrl_c() {
     exit 0
 }
 
+# read -p 'Enter ID: ' id
+# idcheck=$(echo $id | wc -w)
+
 if [ "$#" -ne 1 ]; then
   echo "Error: parameter problem" 
     exit 1
+
 
 elif [ -p $1.pipe ]; then
   echo "Error. That ID already exists" 
   exit 2
 
-else 
+else
+    ./P.sh "$1"
+    if [ -p $1.pipe ]; then
+ 	 echo "Error. That ID already exists" 
+ 	 exit 2 
+    fi
+
     id=$1
     mkfifo $id.pipe
+    ./V.sh "$1"
+   
     echo "A command must begin with create_database, create_table, insert, select, exit or shutdown"
     while true; do
         read -p 'Enter command: ' input
@@ -34,42 +46,41 @@ else
             exit 0
         fi
         if [[ "$request" != 'create_database' ]] && 
-           [[ "$request" != 'create_table' ]] && 
-           [[ "$request" != 'select' ]] && 
-           [[ "$request" != 'insert' ]] &&
-           [[ "$request" != 'shutdown' ]]; then
-
-             echo "A command must begin with create_database, create_table, insert, select, exit or shutdown"
+        [[ "$request" != 'create_table' ]] && 
+        [[ "$request" != 'select' ]] && 
+        [[ "$request" != 'insert' ]] &&
+        [[ "$request" != 'shutdown' ]]; then
+          echo "A command must begin with create_database, create_table, insert, select, exit or shutdown"
+        
         else
-          if [ -p server.pipe ]; then 
-                  echo "$serverRequest" > server.pipe
-                  while true; do
-                
-                      read pipe_input
-                      case $pipe_input in
-                      Error:*)	
-                          echo $pipe_input
-                          break
-                        ;;
-                      OK*)
-                          echo $pipe_input
-                          break
-                          ;;
-                      start_result*)
-                          for line in $pipe_input; do
-                            if [[ "$line" != 'start_result' ]] && 
-                               [[ "$line" != 'end_result' ]]; then
-                                 
-                                 echo $line
-                            fi
-                          done
-                          break
-                          ;;
-                      esac
-                  done < $id.pipe
-          else 
-            echo "Unable to connect to server"
-          fi
-      fi
-    done
+	    if [ -p server.pipe ]; then 
+            	echo "$serverRequest" > server.pipe
+            	while true; do
+             
+                	read pipe_input
+                	case $pipe_input in
+               		Error:*)	
+                  		echo $pipe_input
+                  		break
+                 		;;
+                	OK*)
+                  		echo $pipe_input
+                  		break
+                  		;;
+                	start_result*)
+                  		for line in $pipe_input; do
+                    		if [[ "$line" != 'start_result' ]] && 
+                               	[[ "$line" != 'end_result' ]]; then
+                         		echo $line
+                    		fi
+                  		done
+                  		break
+                  		;;
+               		esac
+            	done < $id.pipe
+	     else 
+		echo "Unable to connect to server"
+	     fi
+        fi
+      done
 fi
